3847de7c98ac205a59358e7a527d0442f86ae264
Jan WALSH <jwalsh@e3r6p6.42.fr>
2017-02-18 12:04:20 +0100

    updated vec3 to rot matrix

--- vec3_to_rot_matrix.c
+++ vec3_to_rot_matrix.c
@@ -6,33 +6,37 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 13:47:18 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/02/10 14:03:28 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/02/18 12:03:48 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libmathft.h"
 
 /*
-** Calculates a rotation matrix based on two vectors.
+** Calculates a rotation matrix to transform vector v1 into vector v2.
 */
 
 t_matrix4 vec3_to_rot_matrix(t_vec3 v1, t_vec3 v2)
 {
-    t_matrix4    new_matrix;
-    t_vec3      tmp;
+    t_vec3      xaxis;
+    t_vec3      yaxis;
+    t_matrix4   m;
+    
+	xaxis = vec3_cross_product(v1, v2);
+	xaxis = vec3_normalize(xaxis);
 
-    new_matrix = new_identity_matrix4();
-    tmp = vec3_normalize(v1);
-    new_matrix[0][0] = tmp.x;
-    new_matrix[0][1] = tmp.y;
-    new_matrix[0][2] = tmp.z;
-    tmp = vec3_normalize(vec3_cross_product(v1, v2));
-    new_matrix[2][0] = tmp.x;
-    new_matrix[2][1] = tmp.y;
-    new_matrix[2][2] = tmp.z;
-    tmp = vec3_normalize(vec3_cross_product(tmp, v1));
-    new_matrix[1][0] = tmp.x;
-    new_matrix[1][1] = tmp.y;
-    new_matrix[1][2] = tmp.z;
-    return (new_matrix);
+	yaxis = vec3_cross_product(v2, xaxis);
+	yaxis = vec3_normalize(yaxis);
+	m = new_identity_matrix4();
+
+	m[0][0] = xaxis.x;
+	m[1][0] = yaxis.x;
+	m[2][0] = v1.x;
+	m[0][1] = xaxis.y;
+	m[1][1] = yaxis.y;
+	m[2][1] = v1.y;
+	m[0][2] = xaxis.z;
+	m[1][2] = yaxis.z;
+	m[2][2] = v1.z;
+    return (m);
 }
